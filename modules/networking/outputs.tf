3ef94156f4e65531eab373a013d0649460e1a663
Aftab <mdaftabahmed2305@gmail.com>
2026-06-22 17:09:14 -0400

    fix: appGateway_subnet_id output fixed

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -2,7 +2,7 @@ output "vnet_id" {
 value = azurerm_virtual_network.this.id
   
 }
-output " appGateway_subnet_id" {
-    value = azurerm_subnet.this.id
+output "appGateway_subnet_id" {
+    value = azurerm_subnet.this["appGateway-subnet"].id
   
 }
